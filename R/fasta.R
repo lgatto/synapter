d234ec9187c43dddaa749b2ca9eecb604ee6ae82
Laurent Gatto <l.gatto@dnavision.be>
2014-03-25 16:19:14 +0000

    Merge branch 'master' into local-hedgehog
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/synapter@87849 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/fasta.R
+++ R/fasta.R
@@ -1,19 +1,20 @@
-dbUniquePeptideSet <- function(fastafile, verbose = TRUE) {
-  proteins <- as.character(readAAStringSet(fastafile))
-  peptides <- unlist(cleave(proteins, enzym="trypsin", missedCleavages=0),
-                     use.names=FALSE)
-  upeptides <- peptides[!(duplicated(peptides) | duplicated(peptides, fromLast=TRUE))]
-  ## Note that this does however not take pre/post-fix peptides into account
-  ## like "DLIELTESLFR" and "HNPEFTMMELYMAYADYHDLIELTESLFR",
-  ##      "YYGYTGAFR" and "EGYYGYTGAFR"  
-  ## where the first ones are NOT unique!
-  ## Such cases are handled by filtering duplicates in the peptide data
-  if (verbose)
-    message(paste("Fasta file: ", length(proteins), " proteins\n", 
-                  "            ", length(upeptides),
-                  " out of ", length(unique(peptides)),
-                  " tryptic peptides are proteotypic.",
-                  sep = ""))
-  return(upeptides)
+dbUniquePeptideSet <- function(fastafile, missedCleavages, verbose = TRUE) {
+    proteins <- as.character(readAAStringSet(fastafile))
+    peptides <- unlist(cleave(proteins, enzym = "trypsin",
+                              missedCleavages = missedCleavages),
+                       use.names = FALSE)
+    upeptides <- peptides[!(duplicated(peptides) | duplicated(peptides, fromLast=TRUE))]
+    ## Note that this does however not take pre/post-fix peptides into account
+    ## like "DLIELTESLFR" and "HNPEFTMMELYMAYADYHDLIELTESLFR",
+    ##      "YYGYTGAFR" and "EGYYGYTGAFR"  
+    ## where the first ones are NOT unique!
+    ## Such cases are handled by filtering duplicates in the peptide data
+    if (verbose)
+        message(paste("Fasta file: ", length(proteins), " proteins\n", 
+                      "            ", length(upeptides),
+                      " out of ", length(unique(peptides)),
+                      " tryptic peptides are proteotypic.",
+                      sep = ""))
+    return(upeptides)
 }
    
