661a05469bf34328ef94f52a92dadea268014363
Sebastian Gibb <mail@sebastiangibb.de>
2014-04-03 15:25:18 +0100

    add .createMs2SpectrumFromFragments

--- tests/testthat/test_spectrum-functions.R
+++ tests/testthat/test_spectrum-functions.R
@@ -14,6 +14,15 @@ test_that(".createMs2SpectrumFromSpectrumXml", {
                cbind(mass=3:6, intensity=12:15))
 })
 
+test_that(".createMs2SpectrumFromFragments", {
+  df <- data.frame(precursor.leID=c(rep(1, 6), 2, 2),
+                   product.mhp=c(2, NA, 3, 4, 6, 5, 1, 2),
+                   product.inten=c(NA, 11, 12, 13, 15, 14, 1, 2),
+                   stringsAsFactors=FALSE)
+  expect_equal(synapter:::.createMs2SpectrumFromFragments(1, df),
+               cbind(mass=3:6, intensity=12:15))
+})
+
 test_that(".createSpectrum", {
   m <- c(2, NA, 3, 4, 6, 5)
   i <- c(NA, 11, 12, 13, 15, 14)
