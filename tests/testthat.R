4193e9e713d866bb053b3741f45292bac9bfe8de
Sebastian Gibb <mail@sebastiangibb.de>
2014-04-03 15:00:29 +0100

    add testcases

---
+++ tests/testthat.R
@@ -0,0 +1,4 @@
+library(testthat)
+library(synapter)
+
+test_check("synapter")
---
+++ tests/testthat/test_spectrum-functions.R
@@ -0,0 +1,24 @@
+context("spectrum-functions")
+
+test_that(".createMs2SpectrumFromSpectrumXml", {
+  m <- c(2, NA, 3, 4, 6, 5)
+  i <- c(NA, 11, 12, 13, 15, 14)
+
+  assignments <- data.frame(le_id=1:2,
+                            he_id=c("1,2,3,4,6,5", "8,7"),
+                            stringsAsFactors=FALSE)
+  ms2 <- cbind(HE_ID=1:8, Mass=c(2, NA, 3:8), Intensity=c(NA, 11:17))
+
+  expect_equal(synapter:::.createMs2SpectrumFromSpectrumXml(1, ms2,
+                                                            assignments),
+               cbind(mass=3:6, intensity=12:15))
+})
+
+test_that(".createSpectrum", {
+  m <- c(2, NA, 3, 4, 6, 5)
+  i <- c(NA, 11, 12, 13, 15, 14)
+
+  expect_equal(synapter:::.createSpectrum(m, i),
+               cbind(mass=3:6, intensity=12:15))
+})
+
