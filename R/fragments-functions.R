d0bdeef91cf27d2f4e5466f9815da16f314f917a
Sebastian Gibb <mail@sebastiangibb.de>
2014-04-03 17:14:47 +0100

    add .readFragments

--- R/fragments-functions.R
+++ R/fragments-functions.R
@@ -4,3 +4,18 @@
 .filterNeutralLoss <- function(df) {
   return(df[which(df$Neutral.LossType == "None"), ])
 }
+
+#' read final_fragments.csv
+#' @param file file path
+#' @param removeNeutralLoss remove rows with neutral loss != "none"?
+#' @return a data.frame
+.readFragements <- function(file, removeNeutralLoss=FALSE) {
+  df <- read.csv(file, stringsAsFactors=FALSE)
+
+  if (removeNeutralLoss) {
+    return(.filterNeutralLoss(df))
+  } else {
+    return(df)
+  }
+}
+
