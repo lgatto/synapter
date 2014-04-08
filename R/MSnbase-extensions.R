5acc7310769116cf87b74f0910b4ab1080664d64
Sebastian Gibb <mail@sebastiangibb.de>
2014-04-08 18:32:37 +0100

    move .getSpectrum/.getSpectra to MSnbase-extensions

--- R/MSnbase-extensions.R
+++ R/MSnbase-extensions.R
@@ -40,3 +40,24 @@
   return(sum(.commonPeaks(x, y, tolerance=tolerance)))
 }
 
+
+## these functions are only wrapper around some MSnbase functions and
+## should never be part of MSnbase
+
+#' get spectra from MSnExp objects
+#' same functionality like "get" but doesn't throw an error if the key is
+#' not found
+#' @param key character, key
+#' @param msnexp MSnExp object
+#' @return Spectrum2
+.getSpectrum <- function(key, msnexp) {
+  if (exists(key, envir=assayData(msnexp))) {
+    return(msnexp[[key]])
+  } else {
+    return(.createEmptyMsnbaseSpectrum2(key=key))
+  }
+}
+
+.getSpectra <- function(keys, spectralist) {
+  mapply(.getSpectrum, keys, spectralist)
+}
