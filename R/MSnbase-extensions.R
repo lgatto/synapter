1470fbf6561f1be8be32cd622a6163fc67038280
Sebastian Gibb <mail@sebastiangibb.de>
2015-04-04 18:43:53 +0200

    add synapterPlgsAgreement; see #73

--- R/MSnbase-extensions.R
+++ R/MSnbase-extensions.R
@@ -23,3 +23,28 @@
 .sumAllPeakCounts <- function(msexp) {
   sum(unlist(peaksCount(msexp)))
 }
+
+#' handle plgs/synapter agreement see issue 73:
+#' https://github.com/lgatto/synapter/issues/73
+#' TODO: should be a method for MSnSets in future
+#' @noRd
+synapterPlgsAgreement <- function(msnset) {
+
+  i <- grep("synapterPlgsAgreement", colnames(fData(msnset)))
+
+  ## metric a: how often identified
+  fData(msnset)$nIdentified <- rowSums(!is.na(exprs(msnset)))
+
+  ## metric b: how often PLGS/synapter agree
+  fData(msnset)$nAgree <- rowSums(fData(msnset)[, i] == "agree")
+
+  ## metric c: how often PLGS/synapter disagree
+  fData(msnset)$nDisagree <- rowSums(fData(msnset)[, i] == "disagree")
+
+  ## calculate ratio for easier interpretation
+  fData(msnset)$synapterPlgsAgreementRatio <-
+    fData(msnset)$nAgree/fData(msnset)$nIdentified
+
+  msnset
+}
+
