cd01eaa9654fb80dfacf99bd0dbe2c29db3f6349
Sebastian Gibb <mail@sebastiangibb.de>
2015-11-29 20:45:11 +0100

    this time write a few test first

--- tests/testthat/test_requantify.R
+++ tests/testthat/test_requantify.R
@@ -1,5 +1,37 @@
 context("requantify")
 
+test_that("requantifySum, only common isotopes", {
+  f <- data.frame(isotopicDistr.run1="1_0:1000;1_1:100;1_2:10;1_3:1;",
+                  isotopicDistr.run2="1_0:800;1_1:100;1_2:10;1_3:1;",
+                  isotopicDistr.run3="1_0:2000;1_1:200;1_2:20;1_3:2;",
+                  isotopicDistr.run4="1_0:6000;1_1:1000;1_2:100;1_3:10;1_4:1",
+                  stringsAsFactors=FALSE)
+
+  int <- setNames(c(1111, 911, 2222, 7110), colnames(f))
+  sat5000_int <- setNames(c(111, 111, 222, 1110), colnames(f))
+
+  expect_equal(synapter:::.requantifySum(f, Inf), int)
+  expect_equal(synapter:::.requantifySum(f, 5000), sat5000_int)
+})
+
+test_that("requantifySum, all isotopes below threshold", {
+  f <- data.frame(isotopicDistr.run1="1_0:1000;1_1:100;1_2:10;1_3:1;",
+                  isotopicDistr.run2="1_0:800;1_1:100;1_2:10;1_3:1;",
+                  isotopicDistr.run3="1_0:2000;1_1:200;1_2:20;1_3:2;",
+                  isotopicDistr.run4="1_0:6000;1_1:1000;1_2:100;1_3:10;1_4:1",
+                  stringsAsFactors=FALSE)
+
+  int <- setNames(c(1111, 911, 2222, 7111), colnames(f))
+  sat5000_int <- setNames(c(111, 111, 222, 1111), colnames(f))
+
+  expect_equal(synapter:::.requantifySum(f, Inf,
+                                         keepLonelyIsotopes = TRUE),
+               int)
+  expect_equal(synapter:::.requantifySum(f, 5000,
+                                         keepLonelyIsotopes = TRUE),
+               sat5000_int)
+})
+
 test_that("requantifyReferenceRun", {
   f <- data.frame(isotopicDistr.run1="1_0:1000;1_1:100;1_2:10;1_3:1;",
                   isotopicDistr.run2="1_0:800;1_1:100;1_2:10;1_3:1;",
@@ -8,7 +40,7 @@ test_that("requantifyReferenceRun", {
                   stringsAsFactors=FALSE)
 
   int <- setNames(c(1111, 911, 2222, 7111), colnames(f))
-  sat5000_int <- setNames(c(1111, 911, 2222, 11110), colnames(f))
+  sat5000_int <- setNames(c(1111, 911, 2222, 11111), colnames(f))
 
   expect_equal(synapter:::.requantifyReferenceRun(f, Inf), int)
   expect_equal(synapter:::.requantifyReferenceRun(f, 5000), sat5000_int)
