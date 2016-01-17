4b8cb42208da6bf01bb8478b9e90ea30c8c9652c
Sebastian Gibb <mail@sebastiangibb.de>
2016-01-17 11:13:20 +0100

    create .mergePeptideData

--- tests/testthat/test_master-functions.R
+++ tests/testthat/test_master-functions.R
@@ -8,3 +8,23 @@ test_that(".orderForMasterModels", {
                  list(c(2, 3, 1), c(3, 1, 2)))
 })
 
+test_that(".mergePeptideData", {
+    l <- list(A=data.frame(peptide.seq = LETTERS[1:5],
+                           precursor.retT = 1:5,
+                           stringsAsFactors = FALSE),
+              B=data.frame(peptide.seq = LETTERS[c(1:3, 6:10)],
+                           precursor.retT = c(2:3, 10, 1:5)))
+    m <- data.frame(peptide.seq = LETTERS[1:3],
+                    precursor.retT.master = 1:3,
+                    precursor.retT.slave = c(2:3, 10),
+                    deltaRt = c(-1, -1, -7),
+                    stringsAsFactors = FALSE)
+
+    expect_equal(synapter:::.mergePeptideData(l$A, l$B, verbose = FALSE), m)
+    expect_equal(synapter:::.mergePeptideData(l$A, l$B, maxDeltaRt = 5,
+                                              verbose = FALSE), m[-3,])
+    expect_message(synapter:::.mergePeptideData(l$A, l$B, maxDeltaRt = 5,
+                                                verbose = TRUE),
+                   "Ignoring 1 features because deltaRt > 5")
+})
+
