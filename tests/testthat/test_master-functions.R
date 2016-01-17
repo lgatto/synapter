3b9b94631a9f043916ac4a45b031fd46f6305d4a
Sebastian Gibb <mail@sebastiangibb.de>
2016-01-17 11:50:20 +0100

    add .mergeMaster

--- tests/testthat/test_master-functions.R
+++ tests/testthat/test_master-functions.R
@@ -28,3 +28,25 @@ test_that(".mergePeptideData", {
                    "Ignoring 1 features because deltaRt > 5")
 })
 
+test_that(".mergeMaster", {
+   l <- list(A=list(IdentPeptideData=data.frame(
+                peptide.seq = LETTERS[c(1:3, 6:7)],
+                precursor.retT = c(0.2, 0.4, 0.6, 1.2, 1.4),
+                stringsAsFactors = FALSE)),
+             B=list(IdentPeptideData=data.frame(
+                peptide.seq = LETTERS[c(1:2, 4)],
+                precursor.retT = c(0.21, 0.41, 0.81),
+                stringsAsFactors = FALSE)),
+             C=list(IdentPeptideData=data.frame(
+                peptide.seq = LETTERS[c(1:2, 5)],
+                precursor.retT = c(0.22, 0.42, 1.02),
+                stringsAsFactors = FALSE)))
+   m <- data.frame(peptide.seq = LETTERS[c(1:3, 6:7, 4:5)],
+                   precursor.retT = c(0.2, 0.4, 0.6, 1.2, 1.4, 0.8, 1.0),
+                   stringsAsFactors = FALSE)
+   expect_error(synapter:::.mergeMaster(l[1]),
+                "To create a master at least two identification data are needed.")
+   expect_equal(suppressWarnings(
+                  synapter:::.mergeMaster(l, verbose = FALSE)), m,
+                tolerance = 0.02)
+})
