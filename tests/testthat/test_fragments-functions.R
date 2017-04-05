4bca1ebe8c653249ab3163d72f7de81dadedf1e9
Sebastian Gibb <mail@sebastiangibb.de>
2017-04-05 13:03:56 +0200

    add unit test for filterEmptyFragments

--- tests/testthat/test_fragments-functions.R
+++ tests/testthat/test_fragments-functions.R
@@ -9,11 +9,21 @@ test_that(".filterNeutralLoss", {
                    row.names=as.character(1:5),
                    stringsAsFactors=FALSE)
 
-  dfr <- data.frame(foo=c(1, 2, 5),
-                    Neutral.LossType=rep("None", 3),
-                    row.names=as.character(c(1, 2, 5)),
-                    stringsAsFactors=FALSE)
+  dfr <- df[c(1, 2, 5), ]
 
   expect_equal(synapter:::.filterNeutralLoss(df), dfr)
 })
 
+test_that(".filterEmptyFragments", {
+  df <- data.frame(foo=1:5,
+                   Neutral.LossType=c("", "",
+                                      "NeutralLoss_NH3",
+                                      "NeutralLoss_H2O",
+                                      ""),
+                   row.names=as.character(1:5),
+                   stringsAsFactors=FALSE)
+
+  dfr <- df[3:4,]
+
+  expect_equal(synapter:::.filterEmptyFragments(df), dfr)
+})
